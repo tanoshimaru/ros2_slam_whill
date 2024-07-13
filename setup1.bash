dbdc7f4259c95185115b42ed656f928c298150bb
tanoshimaru <p42f2fza@s.okayama-u.ac.jp>
2024-07-13 23:46:58 +0900

    init

---
+++ setup1.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# VLP-16のドライバをインストール
+mkdir -p /workspace/velodyne_ws/src && cd $_
+git clone https://github.com/ros-drivers/velodyne.git -b ros2
+git clone https://github.com/ros/angles.git -b ros2
+git clone https://github.com/ros/diagnostics.git -b ros2
+cd /workspace/velodyne_ws
+colcon build --symlink-install
+echo "source /workspace/velodyne_ws/install/setup.bash" >> ~/.bashrc
